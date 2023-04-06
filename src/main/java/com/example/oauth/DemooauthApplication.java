02df87424f8b74617f7d3aa02404edc24c722198
rshenoy-pc\rshenoy <rshenoy@bp-3.com>
2023-04-06 23:59:06 +0100

    oauth2 with github as third party client

---
+++ src/main/java/com/example/oauth/DemooauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemooauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemooauthApplication.class, args);
+	}
+
+}
